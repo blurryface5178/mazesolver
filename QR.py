706fdfe327e5f44b007d88f5fdedb198186b1c0b
Blurryface5178 <abhijanwasti@gmail.com>
2019-07-28 02:12:32 +0000

    Final changes to code

--- QR.py
+++ QR.py
@@ -16,15 +16,18 @@ def decodeQR(image):
 		#for point in barcode.polygon:	
 		#        points.append(point)
 		(x, y, w, h) = barcode.rect
+		text = barcode.data.decode("utf-8")
 		cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 255), 2)
 		cv2.imshow("Image", image)
-		centerx = x+w/2
-		centery = y+h/2
+		centerx = x,w
+		centery = y,h
+		print(str(text))
+		cv2.putText(image, str(text), (x+w, y), cv2.FONT_HERSHEY_SIMPLEX, .5, (255,0,0), 2)
 
-		QRs.append([centerx,centery])
+		QRs.append([str(text),centerx,centery])
 		
 	
-	#cv2.putText(image, str(text), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 2)
+
 
 	cv2.waitKey(0)
 	return QRs
